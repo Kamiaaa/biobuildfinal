b9e6e28c64f6f81dbf51fa272753e23d400f13c4
Mizan <mail.kazimizan@gmail.com>
2025-07-24 12:39:46 +0600

    Career page added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -48,7 +48,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-white shadow-lg fixed w-full z-50">
+    <nav className="bg-white shadow-lg fixed w-full z-100">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
